2815e629ee6f79bb764f40de85fd42091dc85c8b
Dhruv Amin <dhruvamin92@gmail.com>
2018-05-07 09:20:18 -0700

    Add token super classes

--- contracts/libraries/token/ERC20.sol
+++ contracts/libraries/token/ERC20.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.18;
 
 interface ERC20 {
+	event Approval(address indexed owner, address indexed spender, uint value);
+	event Transfer(address indexed from, address indexed to, uint value);
+
     function totalSupply() public view returns (uint);
     function balanceOf(address owner) public view returns (uint);
     function allowance(address owner, address spender) public view returns (uint);

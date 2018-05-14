3ae53353c792df2d5bd2503a8e76169262a76360
Dhruv Amin <dhruvamin92@gmail.com>
2018-05-14 01:21:52 -0700

    Get metamask log in

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,56 @@
+var Web3 = require('web3');
+
+// production vs. dev
+if (window.location.hostname == '') {
+  network_id = 3;
+} else {
+  network_id = 5777 // ganache - ethereum network ID
+}
+
+// Check for Metamask and show/hide appropriate warnings.
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if ((typeof web3 !== 'undefined') && (web3.givenProvider !== null)) {
+    var web3js = new Web3(web3.currentProvider);
+
+    // Checking if user is logged into an account
+    web3js.eth.getAccounts(function(err, accounts){
+        if (err != null) console.error("An error occurred: "+err);
+        // User is not logged into Metamask
+        else if (accounts.length == 0) {
+          $('#metamask-login').show();
+          console.log("User is not logged in to MetaMask");
+        }
+        // User is logged in to Metamask
+        else {
+          web3js.version.getNetwork((err, net_id) => {
+            console.log(net_id);
+            if (err != null) console.error("An error occurred: "+err);
+
+            // User is on the correct network
+            // Ropsten test network = 3, main net = 1
+            else if (net_id == network_id) {
+              console.log("User is logged in and on correct network");
+              $('#main-content').show();
+              startApp(web3js);
+            }
+
+            // User is not on the right network
+            else {
+              console.log("User is logged in and on WRONG network");
+              $('#metamask-network').show();
+            }
+        })
+      }
+  });
+
+  // User does not have Metamask / web3 provider
+  } else {
+    console.log('No web3? You should consider trying MetaMask!');
+    $('#metamask-install').show();
+  }
+})
+
 function app() {
     if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
     web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
@@ -15,9 +68,18 @@ function app() {
         var contractData = results[0];
         var networkId = results[1];
 
-        // TODO: Why is results[1] always 1???
-        networkId = 5777
+        // TODO: Why is results[1] always 1??? For now override it based on production vs. dev
+        networkId = 5777;
+        
         var accounts = results[2];
+
+        //User not logged into MetaMask
+        if (accounts.length == 0) {
+          $('#metamask-login').show();
+          console.log("User not logged into MetaMask");
+        }
+
+
         userAccount = accounts[0];
 
         console.log(contractData)
@@ -73,6 +135,7 @@ function app() {
 
     })
 }
+
 $(document).ready(app);
 
 

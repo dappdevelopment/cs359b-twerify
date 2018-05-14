2a9d6e396b852d8135a1139f938ca6e489b5d5e6
Dhruv Amin <dhruvamin92@gmail.com>
2018-05-13 17:05:11 -0700

    Moved from hand implemented to OpenZeppelin

---
+++ contracts/ContentToken.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+import import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract ContentToken is ERC721Token, Ownable {
+  string public constant _name = "ContentToken";
+  string public constant _symbol = "CON";
+  uint256 private constant _price = 0.1 ether;
+
+  //token id and metadata 
+
+  function purchaseContent() public payable {
+    require(msg.value == price);
+    _mint(msg.sender, totalSupply());
+  }
+
+  function hasValidAccess(address _owner) public view returns (bool)  {
+    if (balanceOf(_owner) > 1) return true;
+    return false;
+  }
+}
